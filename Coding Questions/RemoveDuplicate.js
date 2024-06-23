6a02ce16e9e36ea44cc304f5cbc7a4f5ef6793c0
YashLT224 <yashv@lambdatest.com>
2024-06-23 15:21:22 +0000

    remove duplicates

--- Coding Questions/RemoveDuplicate.js
+++ Coding Questions/RemoveDuplicate.js
@@ -1,1 +1,16 @@
-fghgfhfghfghfgfg
+const removeDuplicates = (items) => {
+    let uniqueItems = [];
+    for (let i = 0; i <= items.length - 1; i++) {
+      if (uniqueItems.indexOf(items[i]) == -1) {
+        uniqueItems.push(items[i]);
+      }
+    }
+    return uniqueItems;
+  }
+  
+  
+  let uniqueItemsarray = removeDuplicates([1, 2, 3, 2, 1, 2, 3, 4, 5, 7, 3, 2, 2]);
+  for (let item of uniqueItemsarray) {
+    console.log(item); //2,3,4,5,7
+  }
+  
