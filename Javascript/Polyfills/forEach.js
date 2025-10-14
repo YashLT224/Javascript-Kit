137814169a0abe2e8add1f58058dfdb35c33d9c7
Yash Verma <yashv@lambdatest.com>
2025-10-14 13:18:51 +0530

    Create forEach.js

---
+++ Javascript/Polyfills/forEach.js
@@ -0,0 +1,14 @@
+let arr=[1,2,3,4]
+
+Array.prototype.myForEach=function(callback){
+   for(let i=0;i<this.length;i++){
+       callback(this[i],i,this);
+   }
+}
+
+
+arr.myForEach((item)=>{
+    console.log(item)
+})
+ 
+
